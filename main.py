624bb75124d2ae29174898541fba087cdfbb0e41
Calendil <ercan.derekoy@gmail.com>
2025-04-26 22:53:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # main.py
 
 from fastapi import FastAPI
+from functional_memory_db import app as functional_memory_db_app
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Bridge API is live."}
+
+app.mount("/functional-memory-db", functional_memory_db_app)
